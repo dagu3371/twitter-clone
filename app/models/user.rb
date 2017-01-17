20745de784e06ed99a05c84fc73801c718804d8c
David Gu <dagu3371@gmail.com>
2017-01-18 09:37:41 +1100

    Updated Readme

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 	attr_accessor :remember_token
-
+    has_many :microposts, dependent: :destroy
 	  before_save { self.email = email.downcase }
 	  validates :name, presence: true, length: { maximum: 50 }
 	  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -8,6 +8,11 @@ class User < ApplicationRecord
 	  has_secure_password
 	  validates :password, presence:true, length: { minimum: 6 }, allow_nil: true
 
+    # Defines a proto-feed.
+  # See "Following users" for the full implementation.
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
   # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
